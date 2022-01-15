7431a2acee52a874c34198ce92d1dae1640ae101
minggu99 <tuanphamtran99@gmail.com>
2022-01-15 18:24:49 +0700

    update menu ui responsive

--- src/components/topbar/TopBar.jsx
+++ src/components/topbar/TopBar.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import "./TopBar.scss";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import AccessComponent from "../access/AccessComponent";
 import { LOGOUT } from "../../graphql/mutation/user";
 import { useMutation, useSubscription } from "@apollo/client";
@@ -8,12 +8,16 @@ import { ToastContainer, toast } from "react-toastify";
 import { useRecoilValue } from "recoil";
 import { userState } from "../../state/user";
 import { UPLOADED_PROFILEPIC } from "../../graphql/subscription/user";
+import { Button, Dropdown, Modal } from "react-bootstrap";
 
 const TopBar = () => {
   const [logout] = useMutation(LOGOUT);
 
   const user = useRecoilValue(userState);
   const [profilePic, setProfilePic] = useState("");
+  const [show, setShow] = useState(false);
+
+  const navigator = useNavigate();
 
   useSubscription(UPLOADED_PROFILEPIC, {
     onSubscriptionData({
@@ -23,17 +27,17 @@ const TopBar = () => {
         },
       },
     }) {
-      if (user._id === user_id){
+      if (user._id === user_id) {
         setProfilePic(image);
       }
     },
   });
 
   useEffect(() => {
-    if(user){
+    if (user) {
       setProfilePic(user.profilePic);
     }
-  }, [user])
+  }, [user]);
 
   const onLogoutHanler = () => {
     logout({
@@ -52,14 +56,80 @@ const TopBar = () => {
     });
   };
 
+  const handleClose = () => {
+    setShow(false);
+  };
+
+  const handleShow = () => {
+    setShow(true);
+  };
+
+  const onSelectHandler = (e) => {
+    const link = e.target.innerHTML.toString();
+    if(link === "MY POST"){
+      navigator("/my-post/" + (user ? user._id : ""));
+      return;
+    }
+    if(link === "HOME"){
+      navigator("/");
+      return;
+    }
+    navigator(link.toLowerCase());
+  };
+
+  const displayMenuPopup = () => {
+    return (
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>
+            <AccessComponent isLogin={true}>
+              <Link className="link" to="/settings">
+                <img className="topImg" src={profilePic} alt="profile" />
+                <h6>{user? user.username : ""}</h6>
+              </Link>
+            </AccessComponent>
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Dropdown.Item eventKey="1" onClick={onSelectHandler}>
+            HOME
+          </Dropdown.Item>
+          <Dropdown.Item eventKey="2" onClick={onSelectHandler}>ABOUT</Dropdown.Item>
+          <Dropdown.Item eventKey="3" onClick={onSelectHandler}>CONTACT</Dropdown.Item>
+          <AccessComponent isLogin={true}>
+            <Dropdown.Item eventKey="4" onClick={onSelectHandler}>WRITE</Dropdown.Item>
+            <Dropdown.Item eventKey="5" onClick={onSelectHandler}>MY POST</Dropdown.Item>
+            <Dropdown.Item eventKey="5" onClick={onSelectHandler}>SETTINGS</Dropdown.Item>
+            <Dropdown.Item eventKey="6" onClick={onLogoutHanler}>
+              LOGOUT
+            </Dropdown.Item>
+          </AccessComponent>
+          <AccessComponent isLogin={false}>
+            <Dropdown.Item eventKey="7" onClick={onSelectHandler}>LOGIN</Dropdown.Item>
+            <Dropdown.Item eventKey="8" onClick={onSelectHandler}>REGISTER</Dropdown.Item>
+          </AccessComponent>
+        </Modal.Body>
+      </Modal>
+    );
+  };
+
   return (
     <div className="top">
       <ToastContainer />
+      {displayMenuPopup()}
       <div className="topLeft">
         <i className="topIcon fab fa-facebook-square"></i>
         <i className="topIcon fab fa-instagram-square"></i>
         <i className="topIcon fab fa-pinterest-square"></i>
         <i className="topIcon fab fa-twitter-square"></i>
+      </div>
+      <div className="topTitle">
+        <span className="headerTitleSm">Dev & Storys</span>
+      </div>
+      <div className="topBar">
+        <Button variant="primary" onClick={handleShow}>
+          <i className="fas fa-bars"></i>
+        </Button>
       </div>
       <div className="topCenter">
         <ul className="topList">
